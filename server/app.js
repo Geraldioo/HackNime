5f17a610b5613f01a23991e90d616c3c1ab65bd7
Geraldioo <bryangeraldioo@gmail.com>
2024-03-17 01:12:06 +0700

    fix deploy

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const route = require("./routes");
 const app = express();
-const port = process.env.PORT || 3000;
 
 const cors = require("cors");
 app.use(cors());
@@ -16,29 +15,26 @@ app.use(express.json());
 
 app.use(route);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 module.exports = app;
 
-module.exports = {
-  apps: [
-    {
-      name: "Hacknime",
-      script: "./app.js",
-      env: {
-        SECRET_JWT: "kzhayin",
-        PORT: 80,
-        GOOGLE_CLIENT_ID:
-          "69395850274-8h7op1snhdk96ebh6l9r9np01v31lp7v.apps.googleusercontent.com",
-        GOOGLE_CLIENT_SECRET: "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",
-        SERVER_KEY_MIDTRANS: "SB-Mid-server-_C7h2LxQryCJdMPuXmQRobWc",
-        NODEMAILER_EMAIL: "bryangeraldioo@gmail.com",
-        NODEMAILER_PW: "tnlb tnoc gscn gfwx",
-        DATABASE_URL:
-          "postgres://postgres.bndutznlrgkvrgctgmnt:vK1VZX7rWQfqAkTk@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
-      },
-    },
-  ],
-};
+// module.exports = {
+//   apps: [
+//     {
+//       name: "Hacknime",
+//       script: "./app.js",
+//       env: {
+//         SECRET_JWT: "kzhayin",
+//         PORT: 80,
+//         GOOGLE_CLIENT_ID:
+//           "69395850274-8h7op1snhdk96ebh6l9r9np01v31lp7v.apps.googleusercontent.com",
+//         GOOGLE_CLIENT_SECRET: "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",
+//         SERVER_KEY_MIDTRANS: "SB-Mid-server-_C7h2LxQryCJdMPuXmQRobWc",
+//         NODEMAILER_EMAIL: "bryangeraldioo@gmail.com",
+//         NODEMAILER_PW: "tnlb tnoc gscn gfwx",
+//         DATABASE_URL:
+//           "postgres://postgres.bndutznlrgkvrgctgmnt:vK1VZX7rWQfqAkTk@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
+//       },
+//     },
+//   ],
+// };
